7711056ab79d6fca00f4acfad3079f812feac585
TheDiVaZo (server) <thedivazoyoutub@gmail.com>
2025-07-03 15:11:08 +0300

    Доработал команду перезагрузки конфигурации

--- core/src/main/java/ru/bloodmine/bloodmineantirelog/command/ReloadCommand.java
+++ core/src/main/java/ru/bloodmine/bloodmineantirelog/command/ReloadCommand.java
@@ -4,6 +4,7 @@ import co.aikar.commands.BaseCommand;
 import co.aikar.commands.annotation.CommandAlias;
 import co.aikar.commands.annotation.CommandPermission;
 import co.aikar.commands.annotation.Subcommand;
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.YamlConfiguration;
 import ru.bloodmine.bloodmineantirelog.AntiRelog;
@@ -12,6 +13,7 @@ import ru.bloodmine.bloodmineantirelog.utility.StringUtility;
 
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.logging.Level;
 
 @CommandAlias("antirelog|ar|antirelog")
 public class ReloadCommand extends BaseCommand {
@@ -26,12 +28,18 @@ public class ReloadCommand extends BaseCommand {
     @CommandPermission("antirelog.reload")
     public void onReload(CommandSender sender) {
         AntiRelog.getInstance().saveDefaultConfig();
-        AntiRelog.getInstance().reloadConfig();
-        AntiRelog.getInstance().getConfig().setDefaults(YamlConfiguration.loadConfiguration(
-                new InputStreamReader(AntiRelog.getInstance().getResource("config.yml"), StandardCharsets.UTF_8)));
+        try {
+            AntiRelog.getInstance().reloadConfig();
+            AntiRelog.getInstance().getConfig().setDefaults(YamlConfiguration.loadConfiguration(
+                    new InputStreamReader(AntiRelog.getInstance().getResource("config.yml"), StandardCharsets.UTF_8)));
 
-        pvpManager.setConfig(AntiRelog.getInstance().getConfig());
+            pvpManager.setConfig(AntiRelog.getInstance().getConfig());
 
-        sender.sendMessage(StringUtility.getMessage(AntiRelog.getInstance().getConfig().getString("messages.reload")));
+            sender.sendMessage(StringUtility.getMessage(AntiRelog.getInstance().getConfig().getString("messages.reload")));
+        }
+        catch (Exception e) {
+            AntiRelog.getInstance().getLogger().log(Level.SEVERE, "failed to reload config", e);
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cЕБАТЬ! Конфиг умер нахуй блять сосать. Чекай консось уебок!"));
+        }
     }
 }
